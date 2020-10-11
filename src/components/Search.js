a4459c235c4b7de0a1b11c657662c4aed41f7cdb
Pratyushxl7 <pratyushxl7@gmail.com>
2020-10-11 12:33:51 +0530

    Added rendered results set

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,55 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm]= useState('');
+    const [results, setResults]= useState([]);
 
 
     useEffect(()=>{
-        console.log("I run with every single render");
+        const search = async() => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+              params:{
+                  action: 'query',
+                  list: 'search',
+                  origin: '*',
+                  format: 'json',
+                  srsearch: term
+              }  
+            });
+
+            setResults(data.query.search);
+        }
+
+        if(term && !results.length)
+        {
+            search();
+        }
+        else
+        {
+            const timeout_id = setTimeout(()=> {
+                if(term){   //If term exists then only call search
+                    search();
+                }
+            }, 500);
+    
+            return () => {
+                clearTimeout(timeout_id);
+            };
+        }
     }, [term]);
+
+    const renderedResults = results.map((r)=> {return (<div className="item">
+        <div className="right floated content">
+            <a className="ui button" href={`https://en.wikipedia.org?curid=${r.pageid}`}>Go</a>
+        </div>
+        <div key= {r.pageid} className="content">
+            <div className="header">
+                {r.title}
+            </div>
+            <span dangerouslySetInnerHTML={{__html: r.snippet}}></span>
+        </div>
+    </div>)});
 
     return (
         <div>
@@ -15,9 +58,10 @@ const Search = () => {
                     <label>Enter Search Term</label>
                     <input className="input" 
                     value={term} 
-                    onChange={e=> setTerm(e.target.value)}></input>
+                    onChange={e => setTerm(e.target.value)}></input>
                 </div>
             </div>
+    <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 }
